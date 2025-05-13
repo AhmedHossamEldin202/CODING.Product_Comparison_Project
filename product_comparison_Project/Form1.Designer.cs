bf19bf2eeaac2211eec04ad4dc0f8d541cc1b9d2
AhmedHossamEldin202 <ahmedhosamesa20@gmail.com>
2025-05-13 04:19:34 +0300

    init

--- product_comparison_Project/Form1.Designer.cs
+++ product_comparison_Project/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ProductColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SpecColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CustomerColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RatingColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CommentColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(234, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(350, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Reviews Filter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(609, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Data ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductColumn,
+            this.SpecColumn,
+            this.CustomerColumn,
+            this.RatingColumn,
+            this.CommentColumn});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 327);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ProductColumn
+            // 
+            this.ProductColumn.HeaderText = "Product Name";
+            this.ProductColumn.MinimumWidth = 6;
+            this.ProductColumn.Name = "ProductColumn";
+            this.ProductColumn.ReadOnly = true;
+            // 
+            // SpecColumn
+            // 
+            this.SpecColumn.HeaderText = "Specifications";
+            this.SpecColumn.MinimumWidth = 6;
+            this.SpecColumn.Name = "SpecColumn";
+            this.SpecColumn.ReadOnly = true;
+            // 
+            // CustomerColumn
+            // 
+            this.CustomerColumn.HeaderText = "Customer Name";
+            this.CustomerColumn.MinimumWidth = 6;
+            this.CustomerColumn.Name = "CustomerColumn";
+            this.CustomerColumn.ReadOnly = true;
+            // 
+            // RatingColumn
+            // 
+            this.RatingColumn.HeaderText = "Rating";
+            this.RatingColumn.MinimumWidth = 6;
+            this.RatingColumn.Name = "RatingColumn";
+            this.RatingColumn.ReadOnly = true;
+            // 
+            // CommentColumn
+            // 
+            this.CommentColumn.HeaderText = "Comment";
+            this.CommentColumn.MinimumWidth = 6;
+            this.CommentColumn.Name = "CommentColumn";
+            this.CommentColumn.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(795, 425);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SpecColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustomerColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RatingColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CommentColumn;
     }
 }
 
--- product_comparison_Project/Form1.cs
+++ product_comparison_Project/Form1.cs
@@ -16,5 +16,52 @@ namespace product_comparison_Project
         {
             InitializeComponent();
         }
+
+        public class ProductReview
+        {
+            public string ProductName { get; set; }
+            public string Specifications { get; set; }
+            public decimal Price { get; set; }
+            public string UserName { get; set; }
+            public int Rating { get; set; }
+        }
+
+        private List<ProductReview> GetAllReviews()
+        {
+            return new List<ProductReview>
+            { 
+                new ProductReview { ProductName = "هاتف سامسونج", Specifications = "8GB RAM, 128GB", Price = 5000, UserName = "أحمد علي", Rating = 5 },
+                new ProductReview { ProductName = "لابتوب HP", Specifications = "16GB RAM, 512GB SSD", Price = 15000, UserName = "منى حسن", Rating = 2 },
+                new ProductReview { ProductName = "سماعات بلوتوث", Specifications = "ANC, بطارية 12 ساعة", Price = 700, UserName = "سعيد عمر", Rating = 4 },
+            };
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var allReviews = GetAllReviews(); // جلب كل التقييمات
+            List<ProductReview> filtered;
+
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "المنتجات الجيدة":
+                    filtered = allReviews.Where(r => r.Rating >= 4).ToList(); // تقييم جيد
+                    break;
+                case "المنتجات السيئة":
+                    filtered = allReviews.Where(r => r.Rating <= 2).ToList(); // تقييم سيء
+                    break;
+                default:
+                    filtered = allReviews; // كل المنتجات
+                    break;
+            }
+
+            dataGridView1.DataSource = filtered; // عرض النتائج في الجدول
+        }
     }
 }
